e12ab0c59d8c0cf3a72cd37c32825b9c71a6f72c
Pedro R <rochapedro436@gmail.com>
2022-12-15 17:41:36 -0300

    Segundo commit

--- src/backend/dados/Storage.java
+++ src/backend/dados/Storage.java
@@ -1,4 +1,13 @@
 package backend.dados;
 
+import classes.Funcionario;
+import classes.Vendedor;
+
+import java.util.List;
+
 public class Storage {
+    private List<Funcionario> funcionariosPadrao;
+    private List<Vendedor> vendedores;
+
+
 }
